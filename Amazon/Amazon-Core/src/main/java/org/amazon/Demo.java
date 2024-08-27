3f61201be1f3cb37b2da8b4a48a601c546ed67b0
Hey-sindhu <sindhuashok2596@gmail.com>
2024-08-27 22:52:54 +0530

    Update Demo.java

--- Amazon/Amazon-Core/src/main/java/org/amazon/Demo.java
+++ Amazon/Amazon-Core/src/main/java/org/amazon/Demo.java
@@ -2,8 +2,8 @@ package org.amazon;
 
 public class Demo {
     public static void main(String[] args) {
-       String data="Test";
-       String  test = "Test";
+       String data="test";
+       String  test = "test";
 
         System.out.println("Added ");
 
